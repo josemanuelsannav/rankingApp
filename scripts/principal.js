1de616b0fbb32010aae899b61232a47e38e0a9bd
Jose Manuel Sanchez Navarro <josemanuel.sanchez@viewnext.com>
2024-05-10 12:22:35 +0200

    boton en verde

--- scripts/principal.js
+++ scripts/principal.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('player-form');
     const goToView2Button = document.getElementById('go-to-view2');
     const upload_dataButton = document.getElementById("upload");
+    const fileInput = document.getElementById('fileInput');
 
-    form.addEventListener('submit', function(event) {
+
+    form.addEventListener('submit', function (event) {
         event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
         // Obtener los valores de los campos del formulario
@@ -14,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const jugador = {
             nombre: nombre,
             foto: foto,
-            puntuacion:0
+            puntuacion: 0
         };
 
         // Guardar el jugador en el almacenamiento local
@@ -45,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
         contenedorJugadores.innerHTML = '';
 
         // Recorrer todos los jugadores y crear una tarjeta para cada uno
-        jugadores.forEach(function(jugador) {
+        jugadores.forEach(function (jugador) {
             const card = document.createElement('div');
             card.classList.add('card');
 
@@ -68,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             card.appendChild(contenido);
 
-          
+
             // Agregar la tarjeta al contenedor de jugadores
             contenedorJugadores.appendChild(card);
         });
@@ -76,57 +78,61 @@ document.addEventListener('DOMContentLoaded', function() {
     const jugadoresGuardados = JSON.parse(localStorage.getItem('jugadores')) || [];
     mostrarJugadores(jugadoresGuardados);
 
-    goToView2Button.addEventListener('click', function() {
+    goToView2Button.addEventListener('click', function () {
         // Redireccionar a otra vista
         window.location.href = '/ranking'; // Cambia 'otra_vista.html' por la URL de la otra vista
     });
 
-upload_dataButton.addEventListener("click",function () {
-    const fileInput = document.getElementById('fileInput');
-    console.log(fileInput.files);
-    // Verificar si se ha seleccionado un archivo
-    if (fileInput.files.length > 0) {
-        const file = fileInput.files[0];
-        const reader = new FileReader();
+    upload_dataButton.addEventListener("click", function () {
+        const fileInput = document.getElementById('fileInput');
+        console.log(fileInput.files);
+        // Verificar si se ha seleccionado un archivo
+        if (fileInput.files.length > 0) {
+            const file = fileInput.files[0];
+            const reader = new FileReader();
 
-        // Manejar el evento onload para leer el archivo
-        reader.onload = function(event) {
-            try {
-                const jsonData = JSON.parse(event.target.result);
+            // Manejar el evento onload para leer el archivo
+            reader.onload = function (event) {
+                try {
+                    const jsonData = JSON.parse(event.target.result);
 
-                // Almacenar los datos de jugadores y juegos en el localStorage
-                almacenarDatosEnLocalStorage(jsonData.jugadores, 'jugadores');
-                almacenarDatosEnLocalStorage(jsonData.juegos, 'juegos');
-                window.location.reload();
-            } catch (error) {
-                console.error('Error al parsear el archivo JSON:', error);
-            }
-        };
+                    // Almacenar los datos de jugadores y juegos en el localStorage
+                    almacenarDatosEnLocalStorage(jsonData.jugadores, 'jugadores');
+                    almacenarDatosEnLocalStorage(jsonData.juegos, 'juegos');
+                    window.location.reload();
+                } catch (error) {
+                    console.error('Error al parsear el archivo JSON:', error);
+                }
+            };
 
-        // Leer el archivo como texto
-        reader.readAsText(file);
-    } else {
-        console.error('No se ha seleccionado ningún archivo.');
+            // Leer el archivo como texto
+            reader.readAsText(file);
+        } else {
+            console.error('No se ha seleccionado ningún archivo.');
+        }
+    });
+
+    // Función para cargar un archivo JSON desde el dispositivo y almacenar sus datos en localStorage
+
+
+    // Función para almacenar datos JSON en el localStorage
+    function almacenarDatosEnLocalStorage(jsonData, key) {
+        try {
+            // Convertir el objeto JSON a una cadena JSON
+            const jsonString = JSON.stringify(jsonData);
+
+            // Almacenar la cadena JSON en el localStorage bajo la clave proporcionada
+            localStorage.setItem(key, jsonString);
+
+            console.log('Datos almacenados en el localStorage.');
+        } catch (error) {
+            console.error('Error al almacenar datos en el localStorage:', error);
+        }
     }
-});
-    
-// Función para cargar un archivo JSON desde el dispositivo y almacenar sus datos en localStorage
 
-
-// Función para almacenar datos JSON en el localStorage
-function almacenarDatosEnLocalStorage(jsonData, key) {
-    try {
-        // Convertir el objeto JSON a una cadena JSON
-        const jsonString = JSON.stringify(jsonData);
-
-        // Almacenar la cadena JSON en el localStorage bajo la clave proporcionada
-        localStorage.setItem(key, jsonString);
-
-        console.log('Datos almacenados en el localStorage.');
-    } catch (error) {
-        console.error('Error al almacenar datos en el localStorage:', error);
-    }
-}
-
+    fileInput.addEventListener('change', () => {
+        upload_dataButton.classList.add('btn-success'); // Agregar la clase "btn-success" al botón
+        upload_dataButton.classList.remove('btn-secondary'); // Remover la clase "btn-secondary" del botón  
+    });
 });
 
